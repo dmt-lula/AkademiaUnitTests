eb2dc2596ec1d4dd149a82e707f131a45ea2510a
dmt-lula <dmt.lula@lo5.bielsko.pl>
2025-03-23 16:50:16 +0100

    Poprawka w pliku Premia.java w zadaniu 2

--- Zadanie2/Main/Premia.java
+++ Zadanie2/Main/Premia.java
@@ -6,17 +6,24 @@ public static void main (String args[])
 	}
 	public int ppremia (int staz){
 		int premia;
-		if (staz < 4){
+		
+		if (staz < 0){
+			throw new IllegalArgumentException("Staż nie może być mniejszy od 0");
+		}
+		else if (staz < 4){
 			premia=0;
 		}
-		else if(staz<=8){
+		else if (staz <= 8){
 			premia = 50;
 		}
-		else if(staz<=11) {
+		else if (staz <= 11) {
 			premia = 70;
 		}
-		else if(staz<=70){
+		else if (staz <= 70){
 			premia = 100;
+		}
+		else if (staz > 70){
+			throw new IllegalArgumentException("Staż nie może być większy niż 70");
 		}
 		else premia=0;
 		return premia;

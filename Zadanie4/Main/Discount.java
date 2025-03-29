bffc8a98ee000516eff37ad2cbc93ef87af3e2ec
dmt.lula <dmt.lula@Mac-mini-12.local>
2025-03-29 12:20:28 +0100

    Dodanie zadan z 3 zajec z testow

--- Zadanie4/Main/Discount.java
+++ Zadanie4/Main/Discount.java
@@ -1,10 +1,29 @@
 package Zadanie4.Main;
-class Discount{
+public class Discount{
     public static void main (String args[])
         {
         }
         public double calculateDiscount(double price){
-            return price;
+            double rabat;
+            if (price<0){
+                System.out.println("Cena nie moze byc mniejsza od zero");
+            }
+            else if (price<=5000){
+                return rabat=0;
+            }
+            else if(price<=15000){
+                return rabat=30;
+            }
+            else if(price<=15000){
+                return rabat=40;
+            }
+            else if(price>50000){
+                return rabat=80;
+            }
+            else{
+                System.out.println("Niepoprawne dane");
+            }
+            return rabat=0;
         }
 
     }
--- Zadanie4/Test/DiscountTest.java
+++ Zadanie4/Test/DiscountTest.java
@@ -1,5 +1,21 @@
 package Zadanie4.Test;
 
+import Zadanie4.Main.Discount;
+import org.junit.jupiter.params.ParameterizedTest;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class DiscountTest {
-    
+
+        @ParameterizedTest
+        @CsvSource({"6,6","5000,5000","5001,3500","4000,4000"})
+        void calculateDiscountTest(double dOrder, double dPrice){
+            //Arrange
+            Discount app = new Discount();
+            //Act
+            double result app.calculateDiscount(dOrder);
+            //Assert
+            assertEquals(dPrice,result,0.1);
+
+    }
 }
